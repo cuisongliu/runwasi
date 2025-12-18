f7aba2762be1767514f3b784af942c8f2101ab2e
github-action-benchmark <github@users.noreply.github.com>
2025-12-18 02:25:25 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for 5c75842bcd24fee941f5d6c9a3eda4abc9984a10

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766023626513,
+  "lastUpdate": 1766024725398,
   "repoUrl": "https://github.com/cuisongliu/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -18166,6 +18166,52 @@ window.BENCHMARK_DATA = {
             "value": 283909426,
             "range": "± 9558034",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jiaxiao Zhou",
+            "username": "Mossaka",
+            "email": "duibao55328@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "5c75842bcd24fee941f5d6c9a3eda4abc9984a10",
+          "message": "Merge pull request #1050 from containerd/dependabot/github_actions/actions/github-script-8\n\nchore(deps): bump actions/github-script from 7 to 8",
+          "timestamp": "2025-10-15T16:48:36Z",
+          "url": "https://github.com/cuisongliu/runwasi/commit/5c75842bcd24fee941f5d6c9a3eda4abc9984a10"
+        },
+        "date": 1766024721027,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 14652,
+            "unit": "kB",
+            "extra": "shim: 12012 kB\nzygote: 2640 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 66228,
+            "unit": "kB",
+            "extra": "shim: 53796 kB\nzygote: 12432 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 18540,
+            "unit": "kB",
+            "extra": "shim: 15184 kB\nzygote: 3356 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 19704,
+            "unit": "kB",
+            "extra": "shim: 16348 kB\nzygote: 3356 kB"
           }
         ]
       }
